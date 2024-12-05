6fddadd8debd620204854df6244e301eb9c20a94
Francois de Parscau <francois.deparscau@qlik.com>
2024-12-05 17:00:58 +0100

    fix(DPE-107): remove osgi part from cxf , moved to camel-karaf

--- core/src/test/java/org/apache/cxf/databinding/source/XMLStreamDataReaderTest.java
+++ core/src/test/java/org/apache/cxf/databinding/source/XMLStreamDataReaderTest.java
@@ -22,90 +22,27 @@ package org.apache.cxf.databinding.source;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 
-import javax.xml.XMLConstants;
 import javax.xml.namespace.QName;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamReader;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.ls.LSResourceResolver;
-
-import org.xml.sax.InputSource;
-import org.xml.sax.Locator;
-import org.xml.sax.SAXException;
-
-import com.ctc.wstx.msv.W3CSchemaFactory;
-import com.sun.msv.reader.GrammarReaderController2;
-
-import org.apache.cxf.endpoint.Endpoint;
 import org.apache.cxf.interceptor.Fault;
-import org.apache.cxf.message.Exchange;
-import org.apache.cxf.message.ExchangeImpl;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.message.MessageImpl;
-import org.apache.cxf.service.Service;
-import org.apache.cxf.service.ServiceImpl;
-import org.apache.cxf.service.model.MessageInfo;
-import org.apache.cxf.service.model.MessagePartInfo;
-import org.apache.cxf.service.model.ServiceInfo;
 import org.apache.cxf.staxutils.StaxUtils;
-import org.codehaus.stax2.XMLStreamReader2;
-import org.codehaus.stax2.validation.XMLValidationSchema;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 /**
  *
  */
 public class XMLStreamDataReaderTest {
     private static final byte[] DUMMY_DATA = "<ns:dummy xmlns:ns='http://www.apache.org/cxf'/>".getBytes();
-
-    static class LocalController implements GrammarReaderController2 {
-        @Override
-        public LSResourceResolver getLSResourceResolver() {
-            return null;
-        }
-
-        @Override
-        public void error(Locator[] locs, String errorMessage, Exception nestedException) {
-            StringBuffer errors = new StringBuffer();
-            for (Locator loc : locs) {
-                errors.append("in ").append(loc.getSystemId()).append(' ').append(loc.getLineNumber())
-                        .append(':').append(loc.getColumnNumber());
-            }
-            throw new RuntimeException(errors.toString(), nestedException);
-        }
-
-        @Override
-        public void warning(Locator[] locs, String errorMessage) {
-            StringBuffer errors = new StringBuffer();
-            for (Locator loc : locs) {
-                errors.append("in ").append(loc.getSystemId()).append(' ').append(loc.getLineNumber())
-                        .append(':').append(loc.getColumnNumber());
-            }
-            // no warning allowed.
-            throw new RuntimeException("warning: " + errors.toString());
-        }
-
-        @Override
-        public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
-            return null;
-        }
-    }
+    private static final byte[] DUMMY_DATA_NULL_BYTE =
+        "<ns:dummy xmlns:ns='http://www.apache.org/cxf&#x00;'/>".getBytes();
 
     @Test
     public void testCloseOriginalInputStream() throws Exception {
@@ -129,79 +66,30 @@ public class XMLStreamDataReaderTest {
         assertTrue(in1.isClosed());
     }
 
+    // Parse some XML with a null byte and check we don't return internal Woodstox package names in the error message
     @Test
-    public void testValid() throws Exception {
-        testValidate("resources/schema.xsd", "resources/test-valid.xml", false);
-    }
-
-    @Test
-    public void testInValid() throws Exception {
-        testValidate("resources/schema.xsd", "resources/test-invalid.xml", true);
-    }
-
-
-    private void testValidate(String schemaPath, String xmlPath, boolean exceptionExpected) throws Exception {
-
-        //create schema
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        documentBuilderFactory.setNamespaceAware(true);
-        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-        URL schemaURI = getClass().getResource(schemaPath);
-        Document wsdl = documentBuilder.parse(schemaURI.openStream());
-        String wsdlSystemId = schemaURI.toExternalForm();
-        DOMSource source = new DOMSource(wsdl);
-        source.setSystemId(wsdlSystemId);
-        source.setSystemId(wsdlSystemId);
-
-        XMLValidationSchema schemaw3c =
-                W3CSchemaFactory.newInstance(XMLValidationSchema.SCHEMA_ID_W3C_SCHEMA).createSchema(schemaURI);
-        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-        Schema schema = schemaFactory.newSchema(schemaURI);
-
+    public void testParseNullByte() throws Exception {
         XMLStreamDataReader reader = new XMLStreamDataReader();
-        reader.setSchema(schema);
-
-
-        InputStream testIS = getClass().getResourceAsStream(xmlPath);
         Message msg = new MessageImpl();
-        Exchange exchange = new ExchangeImpl();
 
-        ServiceInfo serviceInfo =  new ServiceInfo();
+        TestInputStream in1 = new TestInputStream(DUMMY_DATA_NULL_BYTE);
 
-        Endpoint endpoint = mock(Endpoint.class);
-        when(endpoint.isEmpty()).thenReturn(false);
-        when(endpoint.size()).thenReturn(1);
-        when(endpoint.get(XMLValidationSchema.class.getName())).thenReturn(schemaw3c);
+        msg.setContent(InputStream.class, in1);
 
-
-        Service svc = new ServiceImpl(serviceInfo);
-
-        exchange.put(Service.class, svc);
-        exchange.put(Endpoint.class, endpoint);
-
-        msg.setExchange(exchange);
-        msg.setContent(InputStream.class, testIS);
         reader.setProperty(Message.class.getName(), msg);
 
-        XMLInputFactory xmlFactory = XMLInputFactory.newInstance();
-        XMLStreamReader2 xmlStreamReader = (XMLStreamReader2) xmlFactory.createXMLStreamReader(testIS, "utf-8");
+        Object obj = reader.read(new QName("http://www.apache.org/cxf", "dummy"),
+                StaxUtils.createXMLStreamReader(in1), XMLStreamReader.class);
 
-        MessageInfo messageInfo = new MessageInfo(null,
-                MessageInfo.Type.INPUT,
-                new QName("http://www.test.org/services",
-                        "NullTestOperationRequest"));
-        MessagePartInfo messagePartInfo  = new MessagePartInfo(new QName(
-                "http://www.test.org/services", "NullTestOperationRequest"), messageInfo);
-        messagePartInfo.setElement(true);
-        boolean exceptionCaught = false;
+        assertTrue(obj instanceof XMLStreamReader);
+
         try {
-            reader.read(messagePartInfo, xmlStreamReader);
-        } catch (Fault fault) {
-            exceptionCaught = true;
-        }  catch (Exception exc) {
-            fail(exc.getMessage());
+            reader.read((XMLStreamReader) obj);
+        } catch (Fault f) {
+            assertFalse(f.getMessage().contains("com.ctc.wstx"));
         }
-        assertEquals(exceptionExpected, exceptionCaught);
+
+        ((XMLStreamReader)obj).close();
     }
 
     private static class TestInputStream extends ByteArrayInputStream {
